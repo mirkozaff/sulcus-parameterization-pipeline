49a05e4032bb984277cffd5cc9e5f0b3b9975085
mirkozaff <mirko.zaff@gmail.com>
2021-10-04 04:51:56 -0700

    added freesurfer pipeline

--- freesurfer.sh
+++ freesurfer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # script to run FreeSurfer
-export FREESURFER_HOME=$HOME/freesurfer
+export FREESURFER_HOME=/Storage2/freesurfer_dir/freesurfer
 export SUBJECTS_DIR=$FREESURFER_HOME/subjects
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 # run recon all on all the subjects
-python freesurfer_reconall_pipeline.py
+python3 freesurfer_reconall_pipeline.py
--- freesurfer_reconall_pipeline.py
+++ freesurfer_reconall_pipeline.py
@@ -2,25 +2,43 @@ import os
 import os.path as path
 import subprocess
 import datetime
+from multiprocessing import Pool
 
+# Folder with t1 images containing only the *.nii
 T1_DIR = './t1_images'
 
+# Getting the subjects list in the folder
 subjects = [f for f in os.listdir(T1_DIR)]
 
 output = open("out.txt", "w")
 
-for s in subjects:
-    begin_time = datetime.datetime.now()
+def reconall(subject):
+	begin_time = datetime.datetime.now()
 
-    subject_name = s.split('.')[0]
-    subject_t1_path = os.path.join(T1_DIR, s)
+	subject_name = subject.split('.')[0]
+	subject_t1_path = os.path.join(T1_DIR, subject)
 
-    output.write(f'SUBJECT: {subject_name} inizio: {begin_time}')
+	# Write on subject log files
+	output.write(f'SUBJECT: {subject_name} inizio: {begin_time}\n')
+	output.flush()
 
-    print(f'RUNNING: recon-all -s {subject_name} -i {subject_t1_path} -all')
-    subprocess.run(['recon-all', '-s', subject_name, '-i', subject_t1_path, '-all'])
+	# Run freesurfer recon-all pipeline
+	print(f'RUNNING: recon-all -s {subject_name} -i {subject_t1_path} -all')
+	subprocess.run(['recon-all', '-s', subject_name, '-i', subject_t1_path, '-all'])
 
-    end_time = datetime.datetime.now() - begin_time
-    output.write(f'SUBJECT: {subject_name} inizio: {end_time}')
+	end_time = datetime.datetime.now() - begin_time
 
-output.close()
+	# Write on subject log files
+	output.write(f'SUBJECT: {subject_name} fine: {end_time}\n')
+	output.flush()
+
+def pool_handler(subjects_list):
+	# Setting multi processing
+	n_processes = 25
+    p = Pool(n_processes)
+	# Inputs: (target function, list_of_inputs)
+    p.map(reconall, subjects_list)
+
+if __name__ == '__main__': 
+	pool_handler(subjects)
+	output.close()
