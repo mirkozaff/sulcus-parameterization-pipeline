2ffa2406ebefb26e6e6ee9a1ca9f7416f66040d2
mirkozaff <mirko.zaff@gmail.com>
2021-10-28 17:31:48 +0200

    morphologist pipeline bv503

--- run_brainvisa_morphologist.py
+++ run_brainvisa_morphologist.py
@@ -6,10 +6,11 @@ DEBUG = True
 if DEBUG:
 	# Main folders testing
 	MAINDIR = './' #Scripts Folder
-	BVDIR = './bv_dir_5.0.2' # Folder containing subjects processed with brainvisa
-	FSDIR = '/home/zaffaro/Desktop/Docker test/docker_bv/fs_subjects' # INPUTS, folder containing subjects processed with freesurfer
+	BVDIR = '/home/zaffaro/Desktop/kki_bv_database' # Folder containing subjects processed with brainvisa
+	#FSDIR = '/home/zaffaro/Desktop/fs_subjects' # INPUTS, folder containing subjects processed with freesurfer
+	FSDIR = '/home/zaffaro/Desktop/sbj2'
 	#BVHOME = 'brainvisa-4.5.0-mandriva'
-	BVHOME = './brainvisa'
+	BVHOME = 'brainvisa-5.0.3'
 	BV_VER = 5
 	scriptDIR = os.path.join(BVDIR, 'BV_scripts')
 else:
@@ -35,14 +36,35 @@ def folder_creation(SUBJECT):
 		os.makedirs(os.path.join(BVDIR, 'subjects', SUBJECT, 't1mri/default_acquisition/default_analysis/segmentation'))
 	if not os.path.exists(scriptDIR):
 		os.makedirs(scriptDIR)
+	DATABASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'database')
+	if not os.path.exists(DATABASE_PATH):
+		create_database(DATABASE_PATH)
 
 
-def import_freesurfer(SUBJECT):
+def create_database(DATABASE_PATH):
+	#CREATE DATABASE
+	file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_create_database.bvproc')
+	file_parsed_template_path = os.path.join(scriptDIR, 'create_database.bvproc')
+	f_in = open(file_template_path, 'r')
+	f_out = open(file_parsed_template_path, 'w')
+
+	for line in f_in:
+		#read replace the string and write to output file
+		line = line.replace('DATABASE_PATH', DATABASE_PATH)
+		f_out.write(line)
+	#close input and output files
+	f_in.close()
+	f_out.close()
+
+	os.system(f'{BVHOME}/bin/bv axon-runprocess {file_parsed_template_path}')
+
+
+def convert_freesurfer_to_brainvisa(SUBJECT):
 	#IMPORT FREESURFER
 	if BV_VER == 4:
 		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_fs_import_4.5_ok.bvproc')
 	else:
-		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_import_freesurfer_bv5.0.bvproc')
+		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/import_freesurfer_bv503.bvproc')
 	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_fs_import_ok.bvproc')
 	f_in = open(file_template_path, 'r')
 	f_out = open(file_parsed_template_path, 'w')
@@ -59,34 +81,17 @@ def import_freesurfer(SUBJECT):
 	f_in.close()
 	f_out.close()
 
-	os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path} --enable-db')
+	#os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path}  --enable-db')
+	os.system(f'{BVHOME}/bin/bv axon-runprocess --enabledb {file_parsed_template_path}')
 
-def run_import_t1mri(SUBJECT):
-	#IMPORT T1MRI
-	file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/import_t1mri_bv5_02.bvproc')
-	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_import_t1mri_bv5_02.bvproc')
-	f_in = open(file_template_path, 'r')
-	f_out = open(file_parsed_template_path, 'w')
-
-	for line in f_in:
-		#read replace the string and write to output file
-		line = line.replace('BVDIR', BVDIR)
-		line = line.replace('SUBJECT', SUBJECT)
-		line = line.replace('BVHOME', BVHOME)	
-		f_out.write(line)
-	#close input and output files
-	f_in.close()
-	f_out.close()
-
-	os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path}')
 
 def run_morphologist(SUBJECT):
 	#MORPHOLOGIST PIPELINE
 	if BV_VER == 4:
 		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_T1_4.5_ok.bvproc')
 	else:
-		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_T1_5.02_ok.bvproc')
-	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_T1.bvproc')
+		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_morphologist_503.bvproc')
+	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_morphologist_503.bvproc')
 	f_in = open(file_template_path, 'r')
 	f_out = open(file_parsed_template_path, 'w')
 
@@ -100,70 +105,7 @@ def run_morphologist(SUBJECT):
 	f_in.close()
 	f_out.close()
 
-	os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path}')
-
-
-def run_left_sulci_recognition(SUBJECT):
-	#LEFT SULCI RECOGNITION
-	file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_left_spam.bvproc')
-	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_Lspam.bvproc')
-	f_in = open(file_template_path, 'r')
-	f_out = open(file_parsed_template_path, 'w')
-
-	for line in f_in:
-		#read replace the string and write to output file
-		line = line.replace('BVDIR', BVDIR)
-		line = line.replace('SUBJECT', SUBJECT)
-		line = line.replace('BVHOME', BVHOME)	
-		f_out.write(line)
-	#close input and output files
-	f_in.close()
-	f_out.close()
-
-	os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path}')
-
-
-def run_right_sulci_recognition(SUBJECT):
-	#RIGHT SULCI RECOGNITION
-	file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/template_right_spam.bvproc')
-	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_Rspam.bvproc')
-	f_in = open(file_template_path, 'r')
-	f_out = open(file_parsed_template_path, 'w')
-
-	for line in f_in:
-		#read replace the string and write to output file
-		line = line.replace('BVDIR', BVDIR)
-		line = line.replace('SUBJECT', SUBJECT)
-		line = line.replace('BVHOME', BVHOME)	
-		f_out.write(line)
-	#close input and output files
-	f_in.close()
-	f_out.close()
-
-	os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path}')
-
-
-def run_sulcal_measures(SUBJECT):
-	#SULCAL MEASURES
-	if BV_VER == 4:
-		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/by_subject_sulcal_measures_temp.bvproc')
-	else:
-		file_template_path = os.path.join(MAINDIR, 'brainvisa_templates/by_subject_sulcal_measures_temp_5_02.bvproc')
-	file_parsed_template_path = os.path.join(scriptDIR, f'{SUBJECT}_by_subject_sulcal_measures.bvproc')
-	f_in = open(file_template_path, 'r')
-	f_out = open(file_parsed_template_path, 'w')
-
-	for line in f_in:
-		#read replace the string and write to output file
-		line = line.replace('BVDIR', BVDIR)
-		line = line.replace('SUBJECT', SUBJECT)
-		line = line.replace('BVHOME', BVHOME)	
-		f_out.write(line)
-	#close input and output files
-	f_in.close()
-	f_out.close()
-
-	os.system(f'{BVHOME}/bin/brainvisa -r {file_parsed_template_path}')
+	os.system(f'{BVHOME}/bin/bv axon-runprocess {file_parsed_template_path}')
 
 
 def main():
@@ -179,35 +121,23 @@ def main():
 	
 	if args.list:
 		subjects_path = os.path.join(BVDIR, 'subjects')
-		subjects = [f for f in os.listdir(subjects_path) if (os.path.isdir(os.path.join(subjects_path, f))) and ('KK' in f)]
+		subjects = [f for f in os.listdir(subjects_path) if (os.path.isdir(os.path.join(subjects_path, f)))]
 
 		for sbj in subjects:
 			#Brainvisa pipeline
 			print(f'*** {sbj} Morphologist pipeline started ***\n')
 			folder_creation(sbj)
-			#import_freesurfer(sbj)
-			if BV_VER == 5:
-				run_import_t1mri(sbj)
-			run_morphologist(sbj)
-			if BV_VER == 4:
-				run_left_sulci_recognition(sbj)
-				run_right_sulci_recognition(sbj)
-			run_sulcal_measures(sbj)
+			folder_creation(sbj)
+			convert_freesurfer_to_brainvisa(sbj)
+			#run_morphologist(sbj)
 	else:
 		sbj =  args.subject
-		#sbj = 'EJ2102'
 		
 		#Brainvisa pipeline
 		print(f'*** {sbj} Morphologist pipeline started ***\n')
 		folder_creation(sbj)
-		import_freesurfer(sbj)
-		#if BV_VER == 5:
-		#	run_import_t1mri(sbj)
-		# run_morphologist(sbj)
-		# if BV_VER == 4:
-		# 	run_left_sulci_recognition(sbj)
-		# 	run_right_sulci_recognition(sbj)
-		# run_sulcal_measures(sbj)
+		convert_freesurfer_to_brainvisa(sbj)
+		run_morphologist(sbj)
 
 if __name__ == '__main__':
 	main()
